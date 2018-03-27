45aa3b484d483f619f29d15541bf23855c209049
Luis Arce <kapit4n@users.noreply.github.com>
2018-03-27 15:04:06 -0400

    Removing not used model

--- app/models/Cliente.scala
+++
@@ -1,9 +0,0 @@
-package models
-
-import play.api.libs.json._
-
-case class Cliente(id: Long, name: String, carnet: Int, id_company: Int)
-
-object Cliente {
-  implicit val clienteFormat = Json.format[Cliente]
-}
